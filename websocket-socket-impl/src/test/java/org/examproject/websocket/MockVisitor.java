8c611a987abddb686e60fdc0276f341d2ab34cf8
hiroxpepe <hiroxpepe@gmail.com>
2012-08-23 11:36:43 +0900

    change the namespace.

---
+++ websocket-socket-impl/src/test/java/org/examproject/websocket/MockVisitor.java
@@ -0,0 +1,63 @@
+/* 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.examproject.websocket;
+
+import org.examproject.websocket.SiteVisitor;
+
+/**
+ * @author hiroxpepe
+ */
+public class MockVisitor implements SiteVisitor {
+    
+    private String person;
+    
+    private String country;
+    
+    private String feeling;
+
+    ///////////////////////////////////////////////////////////////////////////
+    // simple bean properties.
+    
+    @Override
+    public String getPerson() {
+        return person;
+    }
+
+    @Override
+    public void setPerson(String person) {
+        this.person = person;
+    }
+
+    @Override
+    public String getCountry() {
+        return country;
+    }
+
+    @Override
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    
+    @Override
+    public String getFeeling() {
+        return feeling;
+    }
+
+    @Override
+    public void setFeeling(String feeling) {
+        this.feeling = feeling;
+    }
+    
+}
---
+++ websocket-socket-impl/src/test/java/org/examproject/websocket/SiteVisitorWebSocketTest.java
@@ -0,0 +1,85 @@
+package org.examproject.websocket;
+
+import java.util.Observable;
+import javax.inject.Inject;
+
+import org.eclipse.jetty.websocket.WebSocket.Connection;
+import org.eclipse.jetty.websocket.WebSocketConnectionD00;
+import org.eclipse.jetty.websocket.WebSocketServletConnectionD00;
+
+import org.examproject.websocket.SiteVisitorWebSocket;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author hiroxpepe
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("test-app-config.xml")
+public class SiteVisitorWebSocketTest {
+
+    @Inject
+    SiteVisitorWebSocket instance;
+
+    /**
+     * Test of onOpen method, of class SiteVisitorWebSocket.
+     */
+//    @Test
+//    public void testOnOpen() {
+//        System.out.println("onOpen");
+//        instance.onOpen(null);
+//    }
+
+    /**
+     * Test of onMessage method, of class SiteVisitorWebSocket.
+     */
+    @Test
+    public void testOnMessage() {
+        System.out.println("onMessage");
+        String message = "";
+        instance.onMessage(message);
+    }
+
+    /**
+     * Test of onClose method, of class SiteVisitorWebSocket.
+     */
+    @Test
+    public void testOnClose() {
+        System.out.println("onClose");
+        int closeCode = 0;
+        String message = "";
+        instance.onClose(closeCode, message);
+    }
+
+    /**
+     * Test of update method, of class SiteVisitorWebSocket.
+     */
+    @Test
+    public void testUpdate() {
+        System.out.println("update");
+        Observable o = null;
+        Object arg = null;
+        instance.update(o, arg);
+    }
+    
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+}
